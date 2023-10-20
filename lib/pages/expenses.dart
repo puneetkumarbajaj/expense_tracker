3144cc702803dd541308c150bf6c2fc904d840df
Puneet Bajaj <puneetbajaj591@gmail.com>
2023-10-20 16:07:30 -0400

    last changes before I sleep :salute:

--- lib/pages/expenses.dart
+++ lib/pages/expenses.dart
@@ -1,10 +1,19 @@
-import 'package:expense_app/types/widgets.dart';
+import 'package:expense_app/data/database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
 
-class Expenses extends WidgetWithTitle{
-  const Expenses({super.key}) : super(title: "Expenses");
+class Expenses  extends StatefulWidget {
+  final String title;
+
+  Expenses({Key? key, this.title = "Expenses"}) : super(key: key);
 
   @override
+  _ExpensesState createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+   @override
   Widget build(BuildContext context) {
     return const Text("Expenses");
   }
